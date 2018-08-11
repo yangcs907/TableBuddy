42c118e8371d236cd79a8f0c93aec2dea2b08b7a
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-08-11 17:31:32 -0400

    changes

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'reactbuilder.auth0.com',
-  clientId: 'oD5ZdunOBVaSjpBaCZFUd37dkwdoxd9y',
+  domain: 'yangcs907.auth0.com',
+  clientId: '9fDaOqu0k6lEj8lbnP1SuiC3z3BBtN57',
   callbackUrl: 'http://localhost:3000/callback'
 }
