d15e52e8a75f8a2a757380c68ab50e9fac8b0e56
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-08-12 14:31:35 -0400

    changes

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'yangcs907.auth0.com',
-  clientId: '9fDaOqu0k6lEj8lbnP1SuiC3z3BBtN57',
-  callbackUrl: 'https://tablebuddy.herokuapp.com/callback',
-
+  domain: 'reactbuilder.auth0.com',
+  clientId: 'oD5ZdunOBVaSjpBaCZFUd37dkwdoxd9y',
+  callbackUrl: 'http://localhost:3000/callback'
 }
